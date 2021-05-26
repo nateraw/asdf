7c2bcbe736b0debf0137a6077d15748339b24a6d
Nathan Raw <nxr9266@g.rit.edu>
2021-05-25 20:17:04 -0600

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from PIL import Image
 
-this_file_path = Path(__file__)
+this_file_path = Path(__file__).absolute()
 
 def example_image():
     return Image.open(this_file_path.parent / '14.jpg')
