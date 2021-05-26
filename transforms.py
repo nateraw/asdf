570a81314524baa324285c264110e51b30d489fc
Nathan Raw <nxr9266@g.rit.edu>
2021-05-25 18:50:15 -0600

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -1,9 +1,9 @@
 from torchvision.transforms import Compose, Resize, ToTensor, CenterCrop, Normalize, RandomResizedCrop, RandomHorizontalFlip
-from PIL.ImageFile import ImageFile
-from typing import Callable
-from torch import Tensor
+# from PIL.ImageFile import ImageFile
+# from typing import Callable
+# from torch import Tensor
 
-def train_transforms():
+def train_transforms(**kwargs):
     return Compose([
         RandomResizedCrop(224),
         RandomHorizontalFlip(),
@@ -11,9 +11,9 @@ def train_transforms():
         Normalize([0.500, 0.500, 0.500], [0.500, 0.500, 0.500])
     ])
 
-val_transforms = Compose([
-    Resize(224),
-    CenterCrop(224),
-    ToTensor(),
-    Normalize([0.500, 0.500, 0.500], [0.500, 0.500, 0.500])
-])
+# val_transforms = Compose([
+#     Resize(224),
+#     CenterCrop(224),
+#     ToTensor(),
+#     Normalize([0.500, 0.500, 0.500], [0.500, 0.500, 0.500])
+# ])
