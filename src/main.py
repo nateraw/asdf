69296f0ea7c2cd3f6385f319a9a302de0582e4d5
Nathan Raw <nxr9266@g.rit.edu>
2021-05-25 19:34:08 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,3 +2,12 @@ from typing import List
 
 def run(text: str, limit: int = 10) -> List[str]:
     return text.split()[:limit]
+
+class FnWrap:
+    def __init__(self, fn):
+        self.fn = fn
+    def __call__(self, *args, **kwargs):
+        return self.fn(*args, **kwargs)
+
+def wrapped_run(*args, **kwargs):
+    return FnWrap(run)
