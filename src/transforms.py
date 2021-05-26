7814b9c2fec7cc17616761d66480f94c9445166d
Nathan Raw <nxr9266@g.rit.edu>
2021-05-25 19:23:22 -0600

    Update transforms.py

--- src/transforms.py
+++ src/transforms.py
@@ -1,10 +1,23 @@
-from torchvision.transforms import Compose, Resize, ToTensor, CenterCrop, Normalize, RandomResizedCrop, RandomHorizontalFlip
+from torchvision.transforms import Compose, Resize, Scale, ToTensor, CenterCrop, Normalize, RandomResizedCrop, RandomHorizontalFlip
 
 
-def train_transforms():
+def imagenet_normalize():
+    return Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+
+def imagenet_train_transforms():
     return Compose([
         RandomResizedCrop(224),
         RandomHorizontalFlip(),
         ToTensor(),
-        Normalize([0.500, 0.500, 0.500], [0.500, 0.500, 0.500])
+        imagenet_normalize()
+    ])
+
+
+def imagenet_val_transforms():
+    return Compose([
+        Scale(256),
+        CenterCrop(224),
+        ToTensor(),
+        imagenet_normalize,
     ])
